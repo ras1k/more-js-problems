60873e3f0a1aca83384cf56f08b1dc7417199235
ras1k <rasikabdullah@gmail.com>
2023-03-06 16:52:23 +0600

    first commit

---
+++ slice.js
@@ -0,0 +1,8 @@
+const friends = [12,13,14,15,16,69];
+const sliced = friends.slice(2,4);
+console.log(sliced);
+console.log(friends);
+
+const spliced = friends.splice(2,1);
+console.log(spliced);
+console.log(friends);
